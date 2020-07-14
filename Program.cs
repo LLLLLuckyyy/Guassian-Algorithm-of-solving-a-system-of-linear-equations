880b505c1086cb130e6bf7344efa45c6adce284f
Mikhail Chelpanov <gimbarrworkout17@gmail.com>
2020-07-14 13:25:56 +0300

    Added      Guassian Algorithm of solving a system of linear equations.csproj Added      Guassian Algorithm of solving a system of linear equations.sln Added      Program.cs Added      bin/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.deps.json Added      bin/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.dll Added      bin/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.exe Added      bin/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.pdb Added      bin/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.runtimeconfig.dev.json Added      bin/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.runtimeconfig.json Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.AssemblyInfo.cs Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.AssemblyInfoInputs.cache Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.assets.cache Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.csproj.CoreCompileInputs.cache Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.csproj.FileListAbsolute.txt Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.csprojAssemblyReference.cache Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.dll Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.exe Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.genruntimeconfig.cache Added      obj/Debug/netcoreapp3.1/Guassian Algorithm of solving a system of linear equations.pdb Added      obj/Guassian Algorithm of solving a system of linear equations.csproj.nuget.dgspec.json Added      obj/Guassian Algorithm of solving a system of linear equations.csproj.nuget.g.props Added      obj/Guassian Algorithm of solving a system of linear equations.csproj.nuget.g.targets Added      obj/project.assets.json Added      obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Guassian_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            double[,] matrix = new double[n, n];
+            double[] vector = new double[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i,j] = rnd.Next(1,10);
+                }
+                vector[i] = rnd.Next(1,15);
+            }
+
+            Display(matrix, vector, n);
+
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int k = i + 1; k < n; k++)
+                    {
+                        matrix[i, k] = matrix[i, k] / matrix[i, i];
+                    }
+                    vector[i] = vector[i] / matrix[i, i];
+                    matrix[i, i] = 1;
+
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        for (int k = i + 1; k < n; k++)
+                        {
+                            matrix[j, k] = matrix[j, k] - matrix[i, k] * matrix[j, i];
+                        }
+                        vector[j] = vector[j] - vector[i] * matrix[j, i];
+                        matrix[j, i] = 0;
+                    }
+                }
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+                return;
+            }
+
+            Display(matrix, vector, n);
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    vector[i] -= vector[j] * matrix[i, j];
+                    matrix[i, j] -= matrix[j, j] * matrix[i, j];
+                }
+            }
+
+            Display(matrix, vector, n);
+        }
+
+
+
+        static void Display(double[,] matrix,double[] vector, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine("=\t" + vector[i]);
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("\n\n\n");
+        }
+    }
+}
